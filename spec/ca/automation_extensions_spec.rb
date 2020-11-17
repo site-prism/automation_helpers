627e7a60b7a9882a2da9ad75c10a0064f4bcec7b
Luke Hill <lukehill_uk@hotmail.com>
2020-11-17 15:06:40 +0000

    Update specs slightly and gemspec

--- spec/ca/automation_extensions_spec.rb
+++ spec/ca/automation_extensions_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Ca::AutomationExtensions do
   it "has a version number" do
     expect(Ca::AutomationExtensions::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
