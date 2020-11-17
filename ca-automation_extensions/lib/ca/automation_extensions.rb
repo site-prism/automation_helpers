db01d25f864f233dbc392659a40f8d545e807196
Luke Hill <lukehill_uk@hotmail.com>
2020-11-17 12:23:48 +0000

    Gem bundle

---
+++ ca-automation_extensions/lib/ca/automation_extensions.rb
@@ -0,0 +1,8 @@
+require "ca/automation_extensions/version"
+
+module Ca
+  module AutomationExtensions
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ ca-automation_extensions/lib/ca/automation_extensions/version.rb
@@ -0,0 +1,5 @@
+module Ca
+  module AutomationExtensions
+    VERSION = "0.1.0"
+  end
+end
