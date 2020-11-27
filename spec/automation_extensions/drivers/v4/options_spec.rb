dd22d3407cf13594066d500b6fbbd55428367266
Luke Hill <lukehill_uk@hotmail.com>
2020-11-27 11:33:11 +0000

    Fix up all remaining specs for Options class, now fully spec compliant

--- spec/automation_extensions/drivers/v4/options_spec.rb
+++ spec/automation_extensions/drivers/v4/options_spec.rb
@@ -1,45 +1,51 @@
 # frozen_string_literal: true
 RSpec.describe AutomationExtensions::Drivers::V4::Options do
+  let(:extension_class_options) { described_class.new(browser) }
+
   describe "#options" do
-    subject { described_class.new(browser).options }
+    subject { extension_class_options.options }
 
     context "for chrome" do
       let(:browser) { :chrome }
 
-      it "returns the correct v4 compliant options" do
-        expect(subject).to eq({})
-      end
+      it { is_expected.to have_attributes({ browser_name: "chrome" }) }
     end
 
     context "for firefox" do
       let(:browser) { :firefox }
 
-      it "returns the correct v4 compliant options" do
-        expect(subject).to eq({})
-      end
+      it { is_expected.to have_attributes({ browser_name: "firefox", log: { level: "info" } }) }
     end
 
     context "for internet explorer" do
       let(:browser) { :internet_explorer }
 
-      it "returns the correct v4 compliant options" do
-        expect(subject).to eq({})
-      end
+      it { is_expected.to have_attributes({ native_events: true, persistent_hover: true }) }
     end
 
     context "for safari" do
       let(:browser) { :safari }
 
-      it "returns the correct v4 compliant options" do
-        expect(subject).to eq({})
-      end
+      it { is_expected.to have_attributes({ browser_name: "safari", automatic_inspection: true }) }
     end
 
     context "for ios" do
       let(:browser) { :ios }
 
-      it "returns the correct v4 compliant options" do
-        expect(subject).to eq({})
+      it { is_expected.to be_empty }
+    end
+
+    context "for android" do
+      let(:browser) { :android }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "for an unsupported browser" do
+      let(:browser) { :foo }
+
+      it "Doesn't work if the browser is not one of the supported browsers" do
+        expect { subject }.to raise_error(NoMethodError)
       end
     end
   end
