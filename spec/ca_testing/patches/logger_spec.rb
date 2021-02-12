65769c9ea5dc3959de1c97e959c1d13b38cb49bc
Luke Hill <lukehill_uk@hotmail.com>
2021-02-12 10:12:45 +0000

    DRY up more logic

--- spec/ca_testing/patches/logger_spec.rb
+++ spec/ca_testing/patches/logger_spec.rb
@@ -3,8 +3,6 @@ RSpec.describe CaTesting::Patches::Logger do
   it_behaves_like "a patch" do
     subject { described_class.new(CaTesting.logger) }
 
-    before do
-      CaTesting.logger.level = :UNKNOWN
-    end
+    before { CaTesting.logger.level = :UNKNOWN }
   end
 end
