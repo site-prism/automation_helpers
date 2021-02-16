3895d75f73c5f8d4c2b9c04e94df79dfcce24385
Luke Hill <lukehill_uk@hotmail.com>
2021-02-16 12:07:37 +0000

    Add link to github ref

--- lib/ca_testing/patches/logger.rb
+++ lib/ca_testing/patches/logger.rb
@@ -16,6 +16,8 @@ module CaTesting
           can return unencoded binary (confusingly called ASCII-8BIT in Ruby).
           Consequently we set the logger to binmode so it doesn't try to encode the data - this would always
           cause errors for non-ASCII characters, whatever the parent encoding is. An example of this is ©.
+
+          See https://github.com/ruby/net-http/issues/14 for a root cause analysis of the Adapter
         DESCRIPTION
       end
 
