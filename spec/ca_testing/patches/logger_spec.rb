7895c2e58cdc4472f827e603999c521b8b65dfb3
Luke Hill <lukehill_uk@hotmail.com>
2021-02-16 12:05:33 +0000

    Remove all testing crud that is no longer needed

--- spec/ca_testing/patches/logger_spec.rb
+++ spec/ca_testing/patches/logger_spec.rb
@@ -1,22 +1,6 @@
 # frozen_string_literal: true
 RSpec.describe CaTesting::Patches::Logger do
   it_behaves_like "a patch" do
-    subject { described_class.new(fake_logger.new) }
-
-    let(:fake_logger) do
-      Class.new do
-        def initialize
-          @logdev = OpenStruct.new(dev: fake_log_device.new)
-        end
-
-        def fake_log_device
-          Struct.new(:foo) do
-            def set_encoding(*_args)
-              :bar
-            end
-          end
-        end
-      end
-    end
+    subject { described_class.new(double) }
   end
 end
