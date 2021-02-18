1eac555cce97873d58119cb9fd93e7b8e84a0a26
Marcelo Nicolosi <celonicolosi@hotmail.com>
2021-02-18 17:16:14 +0000

    Patch to bubble up the httpOnly property of a cookie, done!

--- lib/ca_testing/patches.rb
+++ lib/ca_testing/patches.rb
@@ -2,4 +2,5 @@
 require "ca_testing/patches/base"
 require "ca_testing/patches/logger"
 require "ca_testing/patches/sample"
+require "ca_testing/patches/selenium_manager"
 require "ca_testing/patches/selenium_options"
