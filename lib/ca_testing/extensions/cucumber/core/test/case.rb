b550fb25e4335716771072924239c6eb4a8ad333
Luke Hill <lukehill_uk@hotmail.com>
2021-03-12 14:47:00 +0000

    Add Cucumber Test Case extensions

---
+++ lib/ca_testing/extensions/cucumber/core/test/case.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Cucumber
+  module Core
+    module Test
+      class Case
+        def feature_file_name
+          feature_file_path.split("/").last.split(".").first
+        end
+
+        def feature_file_path
+          location.to_s
+        end
+      end
+    end
+  end
+end
