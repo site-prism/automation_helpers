379be76732fca953c15c1a989897424b03e64b57
Luke Hill <lukehill_uk@hotmail.com>
2021-03-18 13:26:19 +0000

    Remove Object#blank?

--- lib/ca_testing/extensions.rb
+++ lib/ca_testing/extensions.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 require "ca_testing/extensions/capybara/node/element"
 require "ca_testing/extensions/cucumber/core/test/case"
-require "ca_testing/extensions/object"
 require "ca_testing/extensions/string"
