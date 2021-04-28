b9b97bb7318ec8dede660f5bf5a323ff506780ca
Luke Hill <lukehill_uk@hotmail.com>
2021-04-28 14:40:52 +0100

    Add tests

---
+++ spec/ca_testing/extensions/array_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+describe Array do
+  describe "#uniq?" do
+    subject { array.uniq? }
+
+    context "with a blank array" do
+      let(:array) { [] }
+
+      it { is_expected.to be true }
+    end
+
+    context "with a unique array" do
+      let(:array) { [nil, 1, :foo, Class.new] }
+
+      it { is_expected.to be true }
+    end
+
+    context "with a non-unique array" do
+      let(:array) { [nil, 1, :foo, :foo, Class.new] }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  describe "#non_uniq" do
+    subject { array.non_uniq }
+
+    context "with a blank array" do
+      let(:array) { [] }
+
+      it { is_expected.to eq([]) }
+    end
+
+    context "with a unique array" do
+      let(:array) { [nil, 1, :foo, Class.new] }
+
+      it { is_expected.to eq([]) }
+    end
+
+    context "with a non-unique array" do
+      let(:array) { [nil, 1, :foo, :foo, Class.new] }
+
+      it "returns only the items which are duplicated" do
+        expect(subject).to eq([:foo])
+      end
+    end
+  end
+end
