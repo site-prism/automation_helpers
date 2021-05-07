97a85083de19fcf071c6ecb0f6b2d7e2e9d098f5
Luke Hill <lukehill_uk@hotmail.com>
2021-05-07 11:53:32 +0100

    Remove all redundant code, ensure that headless is set

--- lib/ca_testing/drivers/v4/options.rb
+++ lib/ca_testing/drivers/v4/options.rb
@@ -4,74 +4,44 @@ module CaTesting
   module Drivers
     module V4
       class Options
-        attr_reader :browser
-        private :browser
-
-        def self.for(browser)
-          case browser
-          when :chrome;             then ::Selenium::WebDriver::Chrome::Options.new
-          when :firefox;            then ::Selenium::WebDriver::Firefox::Options.new(log_level: "trace")
-          when :edge;               then ::Selenium::WebDriver::Edge::Options.new
-          when :safari;             then ::Selenium::WebDriver::Safari::Options.new(automatic_inspection: true)
-          when :internet_explorer;  then internet_explorer_options
-          else {}
+        class << self
+          def for(browser)
+            initial_options(browser).tap { |opts| opts.headless! if headless? }
           end
-        end
 
-        def initialize(browser)
-          @browser = browser
-        end
+          private
 
-        def options
-          send("#{browser}_options").tap { |opts| opts.headless! if headless? }
-        end
-
-        private
-
-        def chrome_options
-          ::Selenium::WebDriver::Chrome::Options.new
-        end
-
-        # Constantly fire mouseOver events on click actions (Should help mitigate flaky clicks)
-        def self.internet_explorer_options
-          ::Selenium::WebDriver::IE::Options.new(persistent_hover: true).tap do |opts|
-            # This is needed to mitigate a Selenium4/Browserstack issue whereby in Se4
-            # we combine Browser options and Capabilities and merge them. But for some
-            # reason Browserstack insist on giving `internet_explorer` a non-conformant
-            # name `IE`. This then causes huge issues in trying to find a browser that
-            # would match using firstMatch in 2 different ways.
-            #
-            # A Support ticket has been raised with Browserstack to see if they can fix
-            # anything at their end, as this is a bug with their matching protocols
-            # LH - Aug 2020
-            CaTesting.logger.info("Removing `browser_name` key from options payload.")
-            opts.options.delete(:browser_name)
+          def initial_options(browser)
+            case browser
+            when :chrome;             then ::Selenium::WebDriver::Chrome::Options.new
+            when :firefox;            then ::Selenium::WebDriver::Firefox::Options.new(log_level: "trace")
+            when :edge;               then ::Selenium::WebDriver::Edge::Options.new
+            when :safari;             then ::Selenium::WebDriver::Safari::Options.new(automatic_inspection: true)
+            when :internet_explorer;  then internet_explorer_options
+            else {}
+            end
           end
-        end
 
-        def firefox_options
-          ::Selenium::WebDriver::Firefox::Options.new(log_level: "trace")
-        end
+          # Constantly fire mouseOver events on click actions (Should help mitigate flaky clicks)
+          def internet_explorer_options
+            ::Selenium::WebDriver::IE::Options.new(persistent_hover: true).tap do |opts|
+              # This is needed to mitigate a Selenium4/Browserstack issue whereby in Se4
+              # we combine Browser options and Capabilities and merge them. But for some
+              # reason Browserstack insist on giving `internet_explorer` a non-conformant
+              # name `IE`. This then causes huge issues in trying to find a browser that
+              # would match using firstMatch in 2 different ways.
+              #
+              # A Support ticket has been raised with Browserstack to see if they can fix
+              # anything at their end, as this is a bug with their matching protocols
+              # LH - Aug 2020
+              CaTesting.logger.info("Removing `browser_name` key from options payload.")
+              opts.options.delete(:browser_name)
+            end
+          end
 
-        def edge_options
-          ::Selenium::WebDriver::Edge::Options.new
-        end
-
-        # Preload Web Inspector and JavaScript debugger
-        def safari_options
-          ::Selenium::WebDriver::Safari::Options.new(automatic_inspection: true)
-        end
-
-        def android_options
-          {}
-        end
-
-        def ios_options
-          {}
-        end
-
-        def headless?
-          ENV["HEADLESS"] == "true"
+          def headless?
+            ENV["HEADLESS"] == "true"
+          end
         end
       end
     end
