2b1bf5557e393e6cb8e35a879706fe8035af9f62
David Lake <david.lake@citizensadvice.org.uk>
2021-06-04 11:38:49 +0100

    Migrate android capabilities

---
+++ lib/ca_testing/drivers/v4/browserstack/android.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module CaTesting
+  module Drivers
+    module V4
+      module Browserstack
+        class Android
+          def capabilities
+            {
+              "bstack:options" => {
+                 "osVersion" => "10.0",
+                 "deviceName" => "Samsung Galaxy S20",
+                 "realMobile" => "true",
+                 "appiumVersion" => "1.17.0"
+              }
+            }
+          end
+        end
+      end
+    end
+  end
+end
