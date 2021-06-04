c871fabe4e2169e308cb3ac88cb9e3f77bad2766
David Lake <david.lake@citizensadvice.org.uk>
2021-06-04 11:47:58 +0100

    Migrate internet explorer capabilities

---
+++ lib/ca_testing/drivers/v3/browserstack/internet_explorer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module CaTesting
+  module Drivers
+    module V3
+      module Browserstack
+        class InternetExplorer
+          def capabilities
+            {
+              "browserName" => "internet explorer",
+              "bstack:options" => {
+                "ie" => {
+                  "driver" => "3.141.59",
+                  "arch" => "x32"
+              }
+            }
+          end
+        end
+      end
+    end
+  end
+end
