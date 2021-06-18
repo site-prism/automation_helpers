b7acbdfeed10cc1fd8878ab5fe3084f20843c4dc
Luke Hill <lukehill_uk@hotmail.com>
2021-06-18 09:10:17 +0100

    Permit device name to be passed into cTor for ios caps

--- spec/ca_testing/drivers/v4/browserstack/ios_spec.rb
+++ spec/ca_testing/drivers/v4/browserstack/ios_spec.rb
@@ -1,30 +1,31 @@
 # frozen_string_literal: true
 
 RSpec.describe CaTesting::Drivers::V4::Browserstack::Ios do
-  subject(:capabilities) { described_class.new(appium_version).capabilities }
+  subject(:capabilities) { described_class.new(device_name, ios_version).capabilities }
+
+  let(:ios_version) { "12" }
+  let(:device_name) { "iPhone11" }
 
   describe "#capabilities" do
-    context "with a valid appium version" do
-      let(:appium_version) { "12" }
-
+    context "with a valid device / iOS version" do
       it "has correct ios capabilities" do
         expect(capabilities).to eq(
           {
             "bstack:options" => {
-              "deviceName" => "ios",
+              "deviceName" => "iPhone11",
               "realMobile" => "true",
               "appiumVersion" => "1.19.1"
             }
           }
         )
       end
+    end
 
-      context "with an invalid appium version" do
-        let(:appium_version) { "10" }
+    context "with an invalid iOS version" do
+      let(:ios_version) { "10" }
 
-        it "it complains ios version is invalid" do
-          expect { capabilities }.to raise_error(ArgumentError)
-        end
+      it "it complains that the iOS version is invalid" do
+        expect { capabilities }.to raise_error(ArgumentError)
       end
     end
   end
