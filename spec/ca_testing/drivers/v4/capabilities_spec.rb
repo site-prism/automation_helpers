9128f5d157e98cbc5aa5ec3f156268471e1ab009
Luke Hill <lukehill_uk@hotmail.com>
2021-08-23 11:23:17 +0100

    Migrate capabilities specs

---
+++ spec/ca_testing/drivers/v4/capabilities_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+RSpec.describe CaTesting::Drivers::V4::Capabilities do
+  describe ".for" do
+    subject(:capabilities) { described_class.for(browser, device_options) }
+
+    let(:device_options) { {} }
+
+    context "for android" do
+      let(:browser) { :android }
+
+      it "has correct android capabilities" do
+        expect(capabilities).to eq(
+          {
+            "bstack:options" => {
+              "osVersion" => "10.0",
+              "deviceName" => "Samsung Galaxy S20",
+              "realMobile" => "true",
+              "appiumVersion" => "1.17.0"
+            }
+          }
+        )
+      end
+    end
+
+    context "for chrome" do
+      let(:browser) { :chrome }
+
+      it "has correct chrome capabilities" do
+        expect(capabilities).to eq(
+          {
+            "goog:loggingPrefs" => {
+              browser: "ALL",
+              driver: "ALL"
+            }
+          }
+        )
+      end
+    end
+
+    context "for internet_explorer" do
+      let(:browser) { :internet_explorer }
+
+      it "has correct chrome capabilities" do
+        expect(capabilities).to eq(
+          {
+            "browserName" => "internet explorer",
+            "bstack:options" => {
+              "ie" => {
+                "driver" => "3.141.59",
+                "arch" => "x32"
+              }
+            }
+          }
+        )
+      end
+    end
+
+    context "for ios" do
+      let(:browser) { :ios }
+
+      context "with a valid device / iOS version" do
+        it "has correct ios capabilities" do
+          expect(capabilities).to eq(
+            {
+              "bstack:options" => {
+                "deviceName" => "iPhone11",
+                "realMobile" => "true",
+                "appiumVersion" => "1.19.1"
+              }
+            }
+          )
+        end
+      end
+
+      context "with an invalid iOS version" do
+        let(:ios_version) { "10" }
+
+        it "it complains that the iOS version is invalid" do
+          expect { capabilities }.to raise_error(ArgumentError)
+        end
+      end
+    end
+
+    context "for any other browser" do
+      let(:browser) { :foo }
+
+      it { is_expected.to be_empty }
+    end
+  end
+end
