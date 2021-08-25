f010a3d9f9eb460def9a617d8a2bb88b76d29200
Luke Hill <lukehill_uk@hotmail.com>
2021-08-25 10:46:16 +0100

    Update requirements simplify namespace refs

--- lib/ca_testing/drivers/v4.rb
+++ lib/ca_testing/drivers/v4.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "selenium/webdriver/remote"
-
 require "ca_testing/drivers/v4/local"
 require "ca_testing/drivers/v4/remote"
 require "ca_testing/drivers/v4/browserstack"
