9e095f67ea217431aeb5dad6998017fb4cf7f83c
Luke Hill <lukehill_uk@hotmail.com>
2021-09-20 16:07:41 +0100

    Fix up tests for all contexts of #get caching

--- spec/ca_testing/extensions/selenium/webdriver/logs_spec.rb
+++ spec/ca_testing/extensions/selenium/webdriver/logs_spec.rb
@@ -1,25 +1,43 @@
 # frozen_string_literal: true
 
 describe Selenium::WebDriver::Logs do
-  before { session.visit("/sample_page.html") }
-
   let(:session) { Capybara::Session.new(:selenium_chrome) }
-  let(:log_message1) { instance_double(Selenium::WebDriver::LogEntry, 'Log One') }
-  let(:log_message2) { instance_double(Selenium::WebDriver::LogEntry, 'Log Two') }
 
   before do
-    allow_any_instance_of(Selenium::WebDriver::Chrome::Features)
-      .to receive(:log)
-      .with(type)
-      .and_return([log_message1, log_message2])
     allow(session.driver.browser.logs)
       .to receive(:cached_logs)
       .and_call_original
   end
 
-  describe "#log" do
+  describe "#get" do
     context "for browser logging" do
+      before do
+        # This is the extended module on the Bridge that physically processes the log command
+        allow(Selenium::WebDriver::Chrome::Features)
+          .to receive(:log)
+          .with(:browser)
+          .and_return([browser_log, browser_log])
+      end
       let(:type) { :browser }
+      let(:browser_log) { instance_double(Selenium::WebDriver::LogEntry, to_s: "Time - SEV - Test Browser Log Message") }
+
+      it "caches the response from browser logs" do
+        expect(session.driver.browser.logs).to receive(:cached_logs).once
+
+        session.driver.browser.logs.get(type)
+      end
+    end
+
+    context "for driver logging" do
+      before do
+        # This is the extended module on the Bridge that physically processes the log command
+        allow_any_instance_of(Selenium::WebDriver::Chrome::Features)
+          .to receive(:log)
+          .with(:driver)
+          .and_return([driver_log, driver_log])
+      end
+      let(:type) { :driver }
+      let(:driver_log) { instance_double(Selenium::WebDriver::LogEntry, to_s: "Time - SEV - Test Driver Log Message") }
 
       it "caches the response from browser logs" do
         expect(session.driver.browser.logs).to receive(:cached_logs).once
