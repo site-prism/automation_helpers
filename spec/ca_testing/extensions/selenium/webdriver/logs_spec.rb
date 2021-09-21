86339d2c9f42a0987ee354e35d3a319b378d9fc7
Luke Hill <lukehill_uk@hotmail.com>
2021-09-21 09:22:01 +0100

    More DRYing up of specs

--- spec/ca_testing/extensions/selenium/webdriver/logs_spec.rb
+++ spec/ca_testing/extensions/selenium/webdriver/logs_spec.rb
@@ -2,23 +2,23 @@
 
 describe Selenium::WebDriver::Logs do
   let(:session) { Capybara::Session.new(:selenium_chrome) }
+  let(:filepath) { $stdout }
+  let(:log_entry) { instance_double(Selenium::WebDriver::LogEntry, to_s: "Time - SEV - Browser or Driver Message") }
 
   before do
+    # This is the extended module on the Bridge that physically processes the log command
+    allow_any_instance_of(Selenium::WebDriver::Chrome::Features)
+      .to receive(:log)
+      .with(type)
+      .and_return([log_entry, log_entry])
+
     allow(session.driver.browser.logs)
       .to receive(:cached_logs)
       .and_call_original
   end
 
   context "for browser logging" do
-    before do
-      # This is the extended module on the Bridge that physically processes the log command
-      allow_any_instance_of(Selenium::WebDriver::Chrome::Features)
-        .to receive(:log)
-        .with(:browser)
-        .and_return([browser_log, browser_log])
-    end
     let(:type) { :browser }
-    let(:browser_log) { instance_double(Selenium::WebDriver::LogEntry, to_s: "Time - SEV - Test Browser Log Message") }
 
     describe "#get" do
       it "caches the response from browser logs" do
@@ -29,8 +29,6 @@ describe Selenium::WebDriver::Logs do
     end
 
     describe "#write_log_to_file" do
-      let(:filepath) { $stdout }
-
       it "writes the log messages to a filepath" do
         log_messages = capture_stdout do
           session.driver.browser.logs.write_log_to_file(type, filepath)
@@ -42,15 +40,7 @@ describe Selenium::WebDriver::Logs do
   end
 
   context "for driver logging" do
-    before do
-      # This is the extended module on the Bridge that physically processes the log command
-      allow_any_instance_of(Selenium::WebDriver::Chrome::Features)
-        .to receive(:log)
-        .with(:driver)
-        .and_return([driver_log, driver_log])
-    end
     let(:type) { :driver }
-    let(:driver_log) { instance_double(Selenium::WebDriver::LogEntry, to_s: "Time - SEV - Test Driver Log Message") }
 
     describe "#get" do
       it "caches the response from browser logs" do
@@ -61,8 +51,6 @@ describe Selenium::WebDriver::Logs do
     end
 
     describe "#write_log_to_file" do
-      let(:filepath) { $stdout }
-
       it "writes the log messages to a filepath" do
         log_messages = capture_stdout do
           session.driver.browser.logs.write_log_to_file(type, filepath)
