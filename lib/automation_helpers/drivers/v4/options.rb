635aef0f3c5014c61b9566c442eebf711ebd4c20
Luke Hill <lukehill_uk@hotmail.com>
2023-08-17 09:57:51 +0100

    update options class to use non-legacy headless methods

--- lib/automation_helpers/drivers/v4/options.rb
+++ lib/automation_helpers/drivers/v4/options.rb
@@ -11,27 +11,39 @@ module AutomationHelpers
       #
       # #### Initial setup options
       #
-      # - **ENV["HEADLESS"]** (optional) - Whether you want your browser to run headless or not
+      # - **ENV["HEADLESS"]** (optional) - Whether you want your browser (only applicable ones) to run headless or not
       class Options
         class << self
           # @return [Selenium::Webdriver::Options]
           #
           # Returns the Options payload relevant to the browser specified to be passed to the driver instantiation
           def for(browser)
-            initial_options(browser).tap { |opts| opts.headless! if headless? }
+            case browser
+            when :chrome;             then chrome_options
+            when :firefox;            then firefox_options
+            when :edge;               then edge_options
+            when :safari;             then safari_options
+            when :internet_explorer;  then internet_explorer_options
+            else {}
+            end
           end
 
           private
 
-          def initial_options(browser)
-            case browser
-            when :chrome;             then ::Selenium::WebDriver::Chrome::Options.new
-            when :firefox;            then ::Selenium::WebDriver::Firefox::Options.new(log_level: 'trace')
-            when :edge;               then ::Selenium::WebDriver::Edge::Options.new
-            when :safari;             then ::Selenium::WebDriver::Safari::Options.new(automatic_inspection: true)
-            when :internet_explorer;  then internet_explorer_options
-            else {}
-            end
+          def chrome_options
+            ::Selenium::WebDriver::Chrome::Options.new.tap { |opts| opts.add_argument('--headless=new') if headless? }
+          end
+
+          def firefox_options
+            ::Selenium::WebDriver::Firefox::Options.new(log_level: 'trace').tap { |opts| opts.add_argument('-headless') if headless? }
+          end
+
+          def edge_options
+            ::Selenium::WebDriver::Edge::Options.new.tap { |opts| opts.add_argument('--headless=new') if headless? }
+          end
+
+          def safari_options
+            ::Selenium::WebDriver::Safari::Options.new(automatic_inspection: true)
           end
 
           # Constantly fire mouseOver events on click actions (Should help mitigate flaky clicks)
