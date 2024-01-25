64822c9ca1f00cb9acdc2f030743961fd9c0d233
Luke Hill <lukehill_uk@hotmail.com>
2024-01-25 09:09:11 +0000

    Fix logic for not running test

--- spec/automation_helpers/extensions/selenium/webdriver/logs_spec.rb
+++ spec/automation_helpers/extensions/selenium/webdriver/logs_spec.rb
@@ -7,7 +7,7 @@ describe Selenium::WebDriver::Logs do
   let(:logs) { capybara_session.driver.browser.logs }
 
   before do
-    skip 'Test is not compatible on older capybara' if old_capybara? && old_selenium?
+    skip 'Test is not compatible on older gems' if old_capybara? || old_selenium?
 
     allow(bridge)
       .to receive(:log)
