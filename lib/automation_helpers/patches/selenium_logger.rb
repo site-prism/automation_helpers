4324ab0e0aa799721899d63b4b4183c84d7fabc8
Luke Hill <lukehill_uk@hotmail.com>
2024-12-03 09:11:21 +0000

    Update selenium logger to be deprecated

--- lib/automation_helpers/patches/selenium_logger.rb
+++ lib/automation_helpers/patches/selenium_logger.rb
@@ -11,6 +11,17 @@ module AutomationHelpers
 
       def description
         <<~DESCRIPTION
+          UPDATE: 3/12/2024
+          This patch should in theory be redundant now the root cause has been identified as a processing
+          debugger fault in RubyMine. Since Ruby 3.2 this should no longer be an issue providing you are
+          using an "up-to-date" RubyMine. I personally haven't encountered this issue for a couple of years
+
+          As such this patch is now considered a deprecated patch with a 1 year TTL, at which point we'll then
+          consider it for removal from the gem proper
+
+          ~~~~~~~~~~~~~~~~~~~~~
+
+          ORIGINAL ISSUE:
           When using the Selenium Logger that is set to pipe to a file, the Net::HTTP adapter (default),
           can return unencoded binary (confusingly called ASCII-8BIT in Ruby).
           Consequently we set the logger to binmode so it doesn't try to encode the data - this would always
@@ -18,6 +29,14 @@ module AutomationHelpers
 
           See https://github.com/ruby/net-http/issues/14 for a root cause analysis of the Adapter
         DESCRIPTION
+      end
+
+      def deprecation_notice_date
+        Time.new(2025, 12, 3)
+      end
+
+      def prevent_usage_date
+        Time.new(2026, 12, 3)
       end
 
       def perform
