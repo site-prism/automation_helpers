5e55f2130233089952a7f1e981c90622705b8173
Luke Hill <lukehill_uk@hotmail.com>
2024-12-03 09:26:26 +0000

    Re-add missing guard code and prevent logger spec from running also

--- spec/automation_helpers/patches/selenium_logger_spec.rb
+++ spec/automation_helpers/patches/selenium_logger_spec.rb
@@ -2,6 +2,10 @@
 
 RSpec.describe AutomationHelpers::Patches::SeleniumLogger do
   it_behaves_like 'a patch' do
+    before do
+      skip 'Test will not pass on old selenium with semi-old capybara' if incompatible_capybara_and_selenium?
+    end
+
     subject(:patch) { described_class.new }
   end
 end
--- spec/support/browser.rb
+++ spec/support/browser.rb
@@ -9,5 +9,11 @@ module SpecSupport
     def old_selenium?
       Gem::Version.new(Selenium::WebDriver::VERSION) < Gem::Version.new('4.7')
     end
+
+    def incompatible_capybara_and_selenium_versions?
+      Gem::Version.new(Capybara::VERSION) < Gem::Version.new('3.39') &&
+        Gem::Version.new(Selenium::WebDriver::VERSION) > Gem::Version.new('4.10') &&
+        Gem::Version.new(Selenium::WebDriver::VERSION) < Gem::Version.new('4.20')
+    end
   end
 end
